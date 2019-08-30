67e01b08a0b0ba49c1f1717e0da576f094c03bd4
Hoang-Duy <hoangduy02071997@gmail.com>
2019-08-30 14:42:53 +0700

    This is new Code

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import './App.css';
 function App(props) {
   return (
     <div className="App">
-      <div className="App-child">
-        {props.data.map(sp => {
-          return (
-            <>
-              <img src={sp.imgURL}></img>
-              <h3>{sp.name}</h3>
-              <div><span class="span-dollar">{sp.price}</span></div>
-            </>
-          );
-        })}
-      </div>
+      {props.data.map(sp => {
+        return (
+          <div className="App-child">
+            <img src={sp.imgURL}></img>
+            <h3>{sp.name}</h3>
+            <div><span class="span-dollar">{sp.price}</span></div>
+          </div>
+        );
+      })}
     </div>
   )
 }
