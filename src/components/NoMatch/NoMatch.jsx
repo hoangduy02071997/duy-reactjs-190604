d52184fce76acdd18b4af39e0371f4f6111fd4d4
Hoang-Duy <hoangduy02071997@gmail.com>
2019-09-10 21:27:01 +0700

    New

--- src/components/NoMatch/NoMatch.jsx
+++ src/components/NoMatch/NoMatch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './NoMatch.css'
 function NoMatch(){
     return (
         <h1 className="jj">404 Not Found, Please contact to administrator to resolve the problem...</h1>
